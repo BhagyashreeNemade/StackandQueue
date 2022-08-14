3a2d5f25e22021d35fc02a9cfa08d17f984a1cab
BhagyashreeNemade <108821937+BhagyashreeNemade@users.noreply.github.com>
2022-08-14 14:25:49 +0530

    display message

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace StackandQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Stack and Queue using Linked List");
+     
+        }
+    }
+}
