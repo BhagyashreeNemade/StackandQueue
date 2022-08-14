d63df979d18b23742457549b075c48bf7c6b248b
BhagyashreeNemade <108821937+BhagyashreeNemade@users.noreply.github.com>
2022-08-14 14:30:05 +0530

    create a Stack of 56->30->70

--- Program.cs
+++ Program.cs
@@ -5,7 +5,11 @@
         static void Main(string[] args)
         {
             Console.WriteLine(" Stack and Queue using Linked List");
-     
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.Push(70);
+            linkedListStack.Push(30);
+            linkedListStack.Push(56);
+            linkedListStack.Display();
         }
     }
 }
